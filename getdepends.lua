5fa092ca084b4b04a9ad310ac139d6ade641e16e
Eduardo Tongson <ed@gimokod.org>
2012-02-13 14:35:16 +0000

    replace problematic shell functions with lua code
    Signed-off-by: Eduardo Tongson <ed@gimokod.org>

---
+++ getdepends.lua
@@ -0,0 +1,56 @@
+#!/usr/bin/env lua
+io.input(arg[1])
+pkgbuild=io.read("*all")
+
+function table.contains(table, element)
+  for _, value in pairs(table) do
+    if value == element then
+      return true
+    end
+  end
+  return false
+end
+
+depends = string.match(pkgbuild, "depends=%(.-%)")
+makedepends = string.match(pkgbuild, "makedepends=%(.-%)")
+
+fulldepends = {}
+sub = {}
+sub["device-mapper"]      = "lvm2"
+sub["gcc-libs"]           = "gcc"
+sub["boost-libs"]         = "boost"
+sub["libltdl"]            = "libtool"
+sub["libjpeg"]            = "libjpeg-turbo"
+sub["libgl"]              = "mesa"
+sub["libglapi"]           = "mesa"
+sub["libegl"]             = "mesa"
+sub["ati-dri"]            = "mesa"
+sub["intel-dri"]          = "mesa"
+sub["nouveau-dri"]        = "mesa"
+sub["khrplatform-devel"]  = "mesa"
+sub["xorg-server-xvfb"]   = "xorg-server"
+sub["xorg-server-common"] = "xorg-server"
+sub["xorg-server-devel"]  = "xorg-server"
+
+if string.find(pkgbuild, "depends") then
+  for x in string.gmatch(depends, "'([%a-]+)'" ) do
+    if table.contains(fulldepends, x) == false then
+      fulldepends[#fulldepends+1] = x
+    end
+  end
+end
+
+if string.find(pkgbuild, "makedepends") then
+  for y in string.gmatch(makedepends, "'([%a-]+)'" ) do
+    if table.contains(fulldepends, y) == false then
+      fulldepends[#fulldepends+1] = y
+    end
+  end
+end
+
+for i = 1, #fulldepends do
+  output = string.gsub(fulldepends[i], "([%a-]+)", sub)
+  io.write(string.format("%s ", output ))
+end
+
+
---
+++ getpkgnames.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/env lua
+io.input(arg[1])
+pkgbuild=io.read("*all")
+
+if string.find(pkgbuild, "pkgbase") then
+  pkgnames = string.match(pkgbuild, "pkgname=%(.-%)")
+  pkgname = {}
+  for w in string.gmatch(pkgnames, "'([%a-]+)'" ) do
+    pkgname[#pkgname+1] = w
+  end
+  for i = 1, #pkgname do
+    io.write(string.format("%s ", pkgname[i] ))
+  end
+else
+  pkgname = string.match(pkgbuild, "pkgname=([%a-]+)")
+  io.write(string.format("%s", pkgname ))
+end
+
+
