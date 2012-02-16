90296f1eb299c9f375a7381c665aa66d6da1f5ce
Eduardo Tongson <ed@gimokod.org>
2012-02-16 14:55:08 +0000

    fix matching
    Signed-off-by: Eduardo Tongson <ed@gimokod.org>

--- scripts/getpkgnames.lua
+++ scripts/getpkgnames.lua
@@ -5,14 +5,14 @@ pkgbuild=io.read("*all")
 if string.find(pkgbuild, "pkgbase") then
     pkgnames = string.match(pkgbuild, "pkgname=%(.-%)")
     pkgname = {}
-    for w in string.gmatch(pkgnames, "'([%a-_]+)'" ) do
+    for w in string.gmatch(pkgnames, "'([%w%p]+)'" ) do
         pkgname[#pkgname+1] = w
     end
     for i = 1, #pkgname do
         io.write(string.format("%s ", pkgname[i] ))
     end
 else
-    pkgname = string.match(pkgbuild, "pkgname=([%a-_]+)")
+    pkgname = string.match(pkgbuild, "pkgname=([%w%p]+)")
     io.write(string.format("%s", pkgname ))
 end
 
